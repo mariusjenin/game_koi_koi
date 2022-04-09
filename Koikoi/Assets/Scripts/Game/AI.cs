c1b68ced5597d85898650a77646849e246a8dd0c
Jenin Marius <marius.jenin@etu.umontpellier.fr>
2022-04-09 19:28:53 +0200

    petite modifs IA

--- Koikoi/Assets/Scripts/Game/AI.cs
+++ Koikoi/Assets/Scripts/Game/AI.cs
@@ -15,48 +15,43 @@ public class AI : Hand
 
     struct GameStateAI
     {
-
         public struct Action
         {
             public bool playerTarget;
             public ActionPart actPart1;
             public ActionPart actPart2;
         }
-        
+
         public struct ActionPossible
         {
             public bool playerTarget;
             public ActionPart actPart1;
             public List<ActionPart> actParts2;
         }
-        
+
         public struct ActionPart
         {
             public bool pairDone;
             public Card card1;
             public Card card2;
         }
-        
+
         //HAND
         public List<Card> aiCards;
         public List<Card> boardCards;
         public List<Card> playerAndDeckCards;
-        public List<Card> aiAndDeckCards;
-        public List<Card> deckCards;
         public List<Card> aiYakusCards;
         public List<Card> playerYaskusCards;
 
         public GameStateAI(GameStateAI previous)
         {
             playerAndDeckCards = new List<Card>(previous.playerAndDeckCards);
-            aiAndDeckCards = new List<Card>(previous.aiAndDeckCards);
-            deckCards = new List<Card>(previous.deckCards);
             aiCards = new List<Card>(previous.aiCards);
             boardCards = new List<Card>(previous.boardCards);
             aiYakusCards = new List<Card>(previous.aiYakusCards);
             playerYaskusCards = new List<Card>(previous.playerYaskusCards);
         }
-        
+
         public int Utility()
         {
             ScoreManager aism = new ScoreManager(aiYakusCards);
@@ -69,17 +64,16 @@ public class AI : Hand
 
         public void ApplyAction(Action act)
         {
-            
             //Part 1
             if (!act.playerTarget)
             {
                 aiCards.Remove(act.actPart1.card1);
-                aiAndDeckCards.Remove(act.actPart1.card1);
             }
             else
             {
                 playerAndDeckCards.Remove(act.actPart1.card1);
             }
+
             if (act.actPart1.pairDone)
             {
                 if (act.playerTarget)
@@ -92,13 +86,14 @@ public class AI : Hand
                     aiYakusCards.Add(act.actPart1.card1);
                     aiYakusCards.Add(act.actPart1.card2);
                 }
+
                 boardCards.Remove(act.actPart1.card2);
             }
             else
             {
                 boardCards.Add(act.actPart1.card1);
             }
-            
+
             //Part 2
             playerAndDeckCards.Remove(act.actPart2.card1);
             if (act.actPart2.pairDone)
@@ -113,6 +108,7 @@ public class AI : Hand
                     aiYakusCards.Add(act.actPart2.card1);
                     aiYakusCards.Add(act.actPart2.card2);
                 }
+
                 boardCards.Remove(act.actPart2.card2);
             }
             else
@@ -120,14 +116,14 @@ public class AI : Hand
                 boardCards.Add(act.actPart2.card1);
             }
         }
-        
-        public  List<ActionPossible> GetActions(bool player) //false : IA | true : player
+
+        public List<ActionPossible> GetActions(bool player) //false : IA | true : player
         {
-            List<Card> sourceCardPart1 = player?playerAndDeckCards:aiCards;
+            List<Card> sourceCardPart1 = player ? playerAndDeckCards : aiCards;
 
             List<ActionPossible> actions = new List<ActionPossible>();
             List<ActionPart> actionParts1 = new List<ActionPart>();
-            
+
             //Part 1
             for (int i = 0; i < sourceCardPart1.Count; i++)
             {
@@ -136,7 +132,6 @@ public class AI : Hand
                 {
                     if (sourceCardPart1[i].month == boardCards[j].month)
                     {
-                        
                         ActionPart actPart1 = new ActionPart();
                         actPart1.pairDone = true;
                         actPart1.card1 = sourceCardPart1[i];
@@ -158,10 +153,14 @@ public class AI : Hand
             //Part 2
             for (int i = 0; i < actionParts1.Count; i++)
             {
-                List<Card> sourceCardPart2 = new List<Card>(player?aiAndDeckCards:playerAndDeckCards);
-                // List<Card> sourceCardPart2 = new List<Card>(deckCards);
+                List<Card> sourceCardPart2 = new List<Card>(playerAndDeckCards);
+
                 List<Card> board = new List<Card>(boardCards);
-                sourceCardPart2.Remove(actionParts1[i].card1);
+                if (player)
+                {
+                    sourceCardPart2.Remove(actionParts1[i].card1);
+                }
+
                 if (actionParts1[i].pairDone)
                 {
                     board.Remove(actionParts1[i].card2);
@@ -189,6 +188,7 @@ public class AI : Hand
                             found = true;
                         }
                     }
+
                     if (!found)
                     {
                         ActionPart actPart2 = new ActionPart();
@@ -197,8 +197,10 @@ public class AI : Hand
                         actionPossible.actParts2.Add(actPart2);
                     }
                 }
+
                 actions.Add(actionPossible);
             }
+
             return actions;
         }
 
@@ -208,10 +210,11 @@ public class AI : Hand
             public int score;
             public ActionPossible act;
         }
+
         public MinimaxResult Minimax(bool player, int depth, ActionPossible prevActionPossible = new())
         {
             List<ActionPossible> acts = GetActions(player);
-            
+
             if (depth <= 0 || acts.Count <= 0)
             {
                 MinimaxResult mmrTerminal = new MinimaxResult();
@@ -224,7 +227,7 @@ public class AI : Hand
             int score = player ? -2147483648 : 2147483647;
             MinimaxResult mmr = new MinimaxResult();
             mmr.score = score;
-            for (int i = 0; i < acts.Count; i ++)
+            for (int i = 0; i < acts.Count; i++)
             {
                 ActionPossible currActionPossible = acts[i];
                 for (int j = 0; j < currActionPossible.actParts2.Count; j++)
@@ -235,7 +238,7 @@ public class AI : Hand
                     act.actPart2 = currActionPossible.actParts2[j];
                     gsai.ApplyAction(act);
 
-                    MinimaxResult currMmr = gsai.Minimax(!player,depth-1,currActionPossible);
+                    MinimaxResult currMmr = gsai.Minimax(!player, depth - 1, currActionPossible);
                     currMmr.act.actPart1 = currActionPossible.actPart1;
                     if (player)
                     {
@@ -249,26 +252,24 @@ public class AI : Hand
                     }
                 }
             }
+
             return mmr;
         }
-        
     }
-    
+
     public override void CanPlay(bool canPlay)
     {
         base.CanPlay(canPlay);
-        if(canPlay) Play();
+        if (canPlay) Play();
     }
+
     public void Play()
     {
-        if(canPlay)
+        if (canPlay)
         {
-
             GameStateAI gsai = new GameStateAI();
             gsai.aiCards = new List<Card>(this.Cards);
             gsai.playerAndDeckCards = new List<Card>(GameManager.instance.player.Cards.Concat(deck.Cards).ToList());
-            gsai.aiAndDeckCards = new List<Card>(GameManager.instance.ai.Cards.Concat(deck.Cards).ToList());
-            gsai.deckCards = new List<Card>(deck.Cards);
             gsai.aiYakusCards = new List<Card>(this.yakus.Cards);
             gsai.playerYaskusCards = new List<Card>(GameManager.instance.player.yakus.Cards);
             gsai.boardCards = new List<Card>(board.Cards);
@@ -278,9 +279,9 @@ public class AI : Hand
 
             // Au tour du joueur de jouer
             GameManager.instance.HandFinishTurn(this);
-            
-            
-            if(canKoikoi) // Se met � jour tout seul dans GameManager.EndTurn
+
+
+            if (canKoikoi) // Se met � jour tout seul dans GameManager.EndTurn
             {
                 canKoikoi = false;
                 // Affichage de la Décision du koikoi
@@ -295,7 +296,6 @@ public class AI : Hand
 
     private void ExecuteAction(AI.GameStateAI.ActionPossible act)
     {
-        
         //PART 1
         act.actPart1.card1.GetUI().Display();
         if (act.actPart1.pairDone)
@@ -310,27 +310,32 @@ public class AI : Hand
         {
             AddCardToBoard(act.actPart1.card1);
         }
+
         RemoveCard(act.actPart1.card1);
-        
+
         //PART 2
         Card card = GameManager.instance.deck.Draw();
         // Création d'un template image à la position du deck
         Transform deckTransform = GameManager.instance.deck.transform;
-        GameObject gObject = Instantiate(GameManager.instance.template, deckTransform.position, deckTransform.rotation, deckTransform.parent.transform);
+        GameObject gObject = Instantiate(GameManager.instance.template, deckTransform.position, deckTransform.rotation,
+            deckTransform.parent.transform);
         UICard uiCard = gObject.GetComponentInChildren<UICard>();
         card.SetUICard(uiCard);
         uiCard.Init(this, card, gObject.GetComponent<Canvas>());
         card.GetUI().Display();
-        
+
         GameStateAI.ActionPart actPart2 = new GameStateAI.ActionPart();
+        // bool found = false;
         for (int i = 0; i < act.actParts2.Count; i++)
         {
             if (act.actParts2[i].card1.Equals(card))
             {
                 actPart2 = act.actParts2[i];
+                // found = true;
                 break;
             }
         }
+        // Debug.Log("test " + found);
         if (actPart2.pairDone)
         {
             // Anime les deux cartes vers la bonne zone Yakus
@@ -343,7 +348,5 @@ public class AI : Hand
         {
             AddCardToBoard(actPart2.card1);
         }
-
-
     }
 }
